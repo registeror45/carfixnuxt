014c4ec1a82df3ea538153ab715723be09c68d75
registeror45 <33532964+registeror45@users.noreply.github.com>
2025-05-18 02:14:30 +0300

    Delete dist directory

--- dist/_nuxt/D1wNcXwR.js
+++
@@ -1,1 +0,0 @@
-import"./a-2ymmio.js";const t=""+new URL("VK.BPTCqi3S.png",import.meta.url).href,e=""+new URL("Telegram.CWhFhqBc.png",import.meta.url).href;export{t as _,e as a};
--- dist/_nuxt/yRelOyLE.js
+++
@@ -1,1 +0,0 @@
-import"./a-2ymmio.js";const r=""+new URL("ProductImage.CWmNufuo.png",import.meta.url).href;export{r as _};
