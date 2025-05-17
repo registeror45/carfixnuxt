b18084fad42fa6b5de0c696a7946c93a8d9a3416
registeror45 <33532964+registeror45@users.noreply.github.com>
2025-05-17 23:49:57 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,13 +17,6 @@ export default defineNuxtConfig({
       hmr: false // Отключить HMR в продакшене
     }
   },
-  devServer: {
-    https: {
-    key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem'), 'utf8'),
-    cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem'), 'utf8')
-    },
-    host: 'localhost'
-  },
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
