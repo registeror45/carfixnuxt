a1f738b573d37c8945b67925529e4e501a519342
registeror45 <33532964+registeror45@users.noreply.github.com>
2025-05-18 21:53:29 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,4 +22,9 @@ export default defineNuxtConfig({
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
     },
   },
+    nitro: {
+    output: {
+      publicDir: '.output/public' // Стандартный путь для Nuxt 3
+    },
+  },
 })
