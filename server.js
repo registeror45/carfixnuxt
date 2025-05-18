9d17fa8ba94e5d25df4e66b76ac5ec56f9d29f74
registeror45 <33532964+registeror45@users.noreply.github.com>
2025-05-18 03:51:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 443;
 
 // Middleware
 app.use(cors({
-  origin: ['https://accomplished-luck.ru', 'https://accomplished-luck.up.railway.app'], 
+  origin: ['https://accomplished-luck.ru', 'https://carfixnuxt-production.up.railway.app'], 
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' })); // Увеличиваем лимит до 50 МБ
